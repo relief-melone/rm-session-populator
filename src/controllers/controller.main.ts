ca49c69b30e023168340a413d2e55a991d5365f4
Relief Melone <relief.melone@gmail.com>
2020-10-30 11:24:37 +0100

    removed old interface

--- src/controllers/controller.main.ts
+++ src/controllers/controller.main.ts
@@ -1,14 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import ConfigAuthenticator from '../config/config.authenticator';
 import ConfigJWT from '../config/config.jwt';
-import { RequestWithUser } from '../interfaces/RequestWithUser';
 import GetUserByCookie from '../services/getUserByCookie';
 import GetUserFromWebToken from '../services/getUserFromWebToken';
 import UserConfig from '../interfaces/UserConfig';
 
 
 export default (env: UserConfig) => async function(
-  req: RequestWithUser, 
+  req: Request, 
   res: Response, 
   next: NextFunction, 
   getUserFromWebToken=GetUserFromWebToken,
